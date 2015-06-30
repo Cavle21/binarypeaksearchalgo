8b45b2055275744d00505ab058f7839a102af884
Cavle21 <quentin.e.wills@gmail.com>
2015-06-30 18:03:25 -0400

    fixes
    fixed it so that it will now search to the right as well as the left.
    Still won't return value and still won't check to see if the value to
    the right is bigger than the value to the left before continuing.

--- peakearchalgo.php
+++ peakearchalgo.php
@@ -2,7 +2,7 @@
 
 	//simple recursive search of an array to find a peak in an array (if it exists)
 	
-	$arr = array(1, 9, 3, 4, 8, 1, 3, 1, 9, 7, 0, 5);
+	$arr = array(1, 3, 4, 8, 4, 3, 5, 9, 7, 0, 5);
 	
 	print_r($arr); echo "<br/>";
 	
@@ -10,25 +10,22 @@
 	
 	//echo $n;
 	
-	function peakfind ($array, $n) {
+	function peakfind ($array, $left, $right) {
 		//echo "Lets start!";
-		
-		$start = $n / 2;
+		$start = ($left + $right) / 2;
 		
 		$start = floor($start);
 		
 		echo $start; echo "</br>";
 		
 		if ($array[$start] < $array[abs($start - 1 )]){
-			echo "To the left" . " ";
-			$n = $start;
-			peakfind($array, $n);
+			echo "To the left" . "<br> ";
+			peakfind($array,$left, $start+1);
 			
 			
 		} else if ($array[$start] < $array[$start + 1]) {
-			echo "To the Right";
-			//return $array[start];
-			
+			echo "To the Right" . "<br>";
+			peakfind($array, $start+1, $right);
 		} else {
 			echo "this is the peak!";
 			echo $array[$start];
@@ -36,6 +33,8 @@
 		}
 	}
 	
-	echo peakfind($arr, $n)
+	$peak = peakfind($arr, 0, (count($arr)-1));
+	
+	var_dump($peak);
 
 ?>
